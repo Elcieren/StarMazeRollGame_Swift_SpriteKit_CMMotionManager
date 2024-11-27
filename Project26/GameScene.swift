eedaf2caf9e046791f65e007c0ac29b1ccbc4380
Elcieren <elcierenn@gmail.com>
2024-11-27 21:50:22 +0300

    Oyuna level2 getirildi ve duzenlemeler yapildi

--- Project26/GameScene.swift
+++ Project26/GameScene.swift
@@ -31,10 +31,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             scoreLabel.text = "Score: \(score)"
         }
     }
+    
+    var currentLevel = 1
 
     
     
     override func didMove(to view: SKView) {
+        startGame()
+    }
+    
+    
+    func startGame(){
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
@@ -158,6 +165,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let location = touch.location(in: self)
         lastTouchPosition = location
         
+        
+        let tappedNodes = nodes(at: location)
+
+            for node in tappedNodes {
+                if node.name == "gameOver" { // "Tebrikler Oyun Bitti" yazısına tıklanırsa
+                    startNextLevel()
+                }
+            }
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -217,17 +233,146 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             node.removeFromParent()
             score += 1
         } else if node.name == "finish" {
-            node.removeFromParent()
-            gameOver = SKLabelNode(fontNamed: "Chalkduster")
-            gameOver.text = "Tebrikler Oyun Bitti"
-            gameOver.fontColor = .red
-            gameOver.fontSize = 56
-            gameOver.position = CGPoint(x: 500, y: 420)
-            gameOver.zPosition = 2
-            addChild(gameOver)
+            if currentLevel == 1 {
+                        currentLevel = 2
+                        gameOver = SKLabelNode(fontNamed: "Chalkduster")
+                        gameOver.text = "Tebrikler Level 1 Bitti"
+                        gameOver.fontColor = .red
+                        gameOver.fontSize = 56
+                        gameOver.position = CGPoint(x: 500, y: 420)
+                        gameOver.zPosition = 2
+                        gameOver.name = "gameOver"
+                        addChild(gameOver)
+                    } else if currentLevel == 2 {
+                        gameOver = SKLabelNode(fontNamed: "Chalkduster")
+                        gameOver.text = "Oynadiginiz icin Tessekkurler Level 2 Bitti"
+                        gameOver.fontColor = .red
+                        gameOver.fontSize = 26
+                        gameOver.position = CGPoint(x: 500, y: 420)
+                        gameOver.zPosition = 2
+                        gameOver.name = "gameOver"
+                        addChild(gameOver)
+                    }
         }
     }
     
     
+    func loadLevel2(){
+        guard let levelURL = Bundle.main.url(forResource: "level2", withExtension: "txt") else {
+            fatalError("Could not find level1.txt in the app bundle")
+        }
+        
+        guard let levelString = try? String(contentsOf: levelURL) else {
+            fatalError("Could not find level1.txt in the app bundle")
+        }
+        
+        
+        let lines = levelString.components(separatedBy: "\n")
+        
+        for (row, line) in lines.reversed().enumerated() {
+            for (column , letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32 , y: (64 * row) + 32)
+                
+                
+                if letter == "x" {
+                    // load wall
+                    let node = SKSpriteNode(imageNamed: "block")
+                    node.position = position
+                    
+                    node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+                    node.physicsBody?.isDynamic = false
+                    addChild(node)
+                    
+                } else if letter == "v" {
+                    // load vortex
+                    let node = SKSpriteNode(imageNamed: "vortex")
+                    node.name = "vortex"
+                    node.position = position
+                    node.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 1)))
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+
+                    node.physicsBody?.categoryBitMask = CollisionTypes.vortext.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    addChild(node)
+                    
+                } else if letter == "s"{
+                    // load star
+                    let node = SKSpriteNode(imageNamed: "star")
+                    node.name = "star"
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+                    node.physicsBody?.categoryBitMask = CollisionTypes.star.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    node.position = position
+                    addChild(node)
+                } else if letter == "f" {
+                    // load finish point
+                    let node = SKSpriteNode(imageNamed: "finish")
+                    node.name = "finish"
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+                    node.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    node.position = position
+                    addChild(node)
+                } else if letter == " " {
+                    //this is an empty space - do nothing
+                } else {
+                    fatalError("Unknow level letter: \(letter)")
+                }
+            }
+            
+        }
+    }
+    
+    
+    func startNextLevel() {
+        removeAllChildren()
+
+            // Yeni haritayı yükle
+        currentLevel = 2
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.zPosition = 2
+        addChild(scoreLabel)
+        
+        
+        
+        
+        
+        loadLevel2()
+        creatPlayer()
+        
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates()
+            
+    }
+    func resetToLevel1() {
+        // Remove all current nodes
+        removeAllChildren()
+
+            // Start the game from level 1
+            currentLevel = 1  // Reset the level counter
+            startGame()
+            loadLevel()  // Load level 1 again
+            creatPlayer()  // Reset player to the start position
+            score = 0
+    }
     
 }
